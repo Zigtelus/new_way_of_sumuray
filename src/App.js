2ae1ebb193d149cc1fca45161939aa611744821d
Zigtelus <60105330+Zigtelus@users.noreply.github.com>
2022-02-03 20:37:58 +0300

    This is 43 lesson. Mistake in messages
    I see an error in DialogsCreator. There is a send and I don't know how to send it an object with type and text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 import Header from './components/Header/Header';
 import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
@@ -14,12 +15,12 @@ function App(props) {
 
   return (
     <div className="app">
-      
         <Header />
         <Navbar />
         <Routes>
-          <Route path='/profile' element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>}/>
-          <Route path='/dialogs/*' element={<Dialogs messagesPage={props.state.messagesPage} dispatch={props.dispatch}/>}/>
+          <Route path='/profile' element={<Profile store={props.store} />}/>
+          {/* <Route path='/profile' element={<Profile profilePage={props.state.profilePage} store={props.store} dispatch={props.dispatch}/>}/> */}
+          <Route path='/dialogs/*' element={<DialogsContainer store={props.store}/>}/>
           <Route path='/news' element={<News />}/>
           <Route path='/music' element={<Music />}/>
           <Route path='/settings' element={<Settings />}/>
